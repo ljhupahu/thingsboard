d643847c6bd015f63bbabbe179b83ffd81d8655a
hanyuepeng <hanyuepeng@taiergroup.com>
2021-06-25 16:17:33 +0800

    docs(注释部分): k8s部署脚本注释
    新增k8s部署脚本注释
    部署顺序
    $ ./k8s-install-tb.sh –loadDemo
    $ ./k8s-deploy-thirdparty.sh
    $ ./k8s-deploy-resources.sh

--- k8s/k8s-install-tb.sh
+++ k8s/k8s-install-tb.sh
@@ -13,13 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# 安装数据库并执行数据库初始化操作及spring环境信息
 #
 
 function installTb() {
 
     loadDemo=$1
-
+    #设置node节点的启动配置和日志配置信息
     kubectl apply -f common/tb-node-configmap.yml
+    #执行ThingsboardInstallApplication逻辑
     kubectl apply -f common/database-setup.yml &&
     kubectl wait --for=condition=Ready pod/tb-db-setup --timeout=120s &&
     kubectl exec tb-db-setup -- sh -c 'export INSTALL_TB=true; export LOAD_DEMO='"$loadDemo"'; start-tb-node.sh; touch /tmp/install-finished;'
@@ -27,15 +29,17 @@ function installTb() {
     kubectl delete pod tb-db-setup
 
 }
-
+#安装postgres
 function installPostgres() {
 
+    #部署postgres
     kubectl apply -f common/postgres.yml
+    #部署关于数据库的配置信息
     kubectl apply -f common/tb-node-postgres-configmap.yml
-
+    #查看 postgres部署状态
     kubectl rollout status deployment/postgres
 }
-
+#安装postgres和cassandra
 function installHybrid() {
 
     kubectl apply -f common/postgres.yml
@@ -53,8 +57,10 @@ function installHybrid() {
                     };\""
 }
 
+##当传递的脚本个数大于0的时候
 while [[ $# -gt 0 ]]
 do
+##取第一个参数等于key
 key="$1"
 
 case $key in
@@ -74,22 +80,24 @@ if [ "$LOAD_DEMO" == "true" ]; then
 else
     loadDemo=false
 fi
-
+#设定环境变量,决定部署方式
 source .env
-
+#k8s中新建命名空间 thingsboard
 kubectl apply -f common/tb-namespace.yml
+#设置k8s环境变量
 kubectl config set-context $(kubectl config current-context) --namespace=thingsboard
-
+#校验在.env中定义的部署类型
 case $DEPLOYMENT_TYPE in
-        basic)
+        basic) #基础部署
         ;;
-        high-availability)
+        high-availability) #高可用部署
         ;;
         *)
+        #如果部署类型配置的不对的话,输出到标准错误输出流中
         echo "Unknown DEPLOYMENT_TYPE value specified: '${DEPLOYMENT_TYPE}'. Should be either basic or high-availability." >&2
         exit 1
 esac
-
+#执行数据库部署
 case $DATABASE in
         postgres)
             installPostgres
